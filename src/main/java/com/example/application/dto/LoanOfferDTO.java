04fbf2e708464a91383eb8c934dde4446d1a8e10
DashaVav <dashavavilova111002@gmail.com>
2024-07-22 15:46:56 +0300

    remove unnecessary annotations

--- src/main/java/com/example/application/dto/LoanOfferDTO.java
+++ src/main/java/com/example/application/dto/LoanOfferDTO.java
@@ -1,14 +1,12 @@
 package com.example.application.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoanOfferDTO {
