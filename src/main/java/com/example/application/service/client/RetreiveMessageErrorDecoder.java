283a9bd6d09277d6a047894c7cb10e35a83bacab
DashaVav <dashavavilova111002@gmail.com>
2024-07-22 15:02:02 +0300

    add deal exception mapper

--- src/main/java/com/example/application/service/client/RetreiveMessageErrorDecoder.java
+++ src/main/java/com/example/application/service/client/RetreiveMessageErrorDecoder.java
@@ -4,21 +4,26 @@ import com.example.application.exception.DealException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 @Component
+@RequiredArgsConstructor
 public class RetreiveMessageErrorDecoder implements ErrorDecoder {
     private final ErrorDecoder errorDecoder = new Default();
+    private final ConversionService conversionService;
 
     @Override
     public Exception decode(String methodKey, Response response) {
         try (InputStream bodyIs = response.body().asInputStream()) {
             ObjectMapper mapper = new ObjectMapper();
             DealExceptionDTO e = mapper.readValue(bodyIs, DealExceptionDTO.class);
-            throw new DealException(e.getError(), e.getStatus());
+            throw Objects.requireNonNull(conversionService.convert(e, DealException.class));
         } catch (IOException e) {
             return errorDecoder.decode(methodKey, response);
         }
