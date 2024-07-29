e5dd71b9f0ecf81406b61b403edea827e4cf2c6a
DashaVav <dashavavilova111002@gmail.com>
2024-07-29 16:41:40 +0300

    add tests for controller

---
+++ src/test/java/com/example/application/controller/ApplicationControllerImplTest.java
@@ -0,0 +1,66 @@
+package com.example.application.controller;
+
+import com.example.application.controller.impl.ApplicationControllerImpl;
+import com.example.application.dto.LoanApplicationRequestDTO;
+import com.example.application.dto.LoanOfferDTO;
+import com.example.application.service.DealService;
+import com.example.application.service.PrescoringService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplicationControllerImplTest {
+    @InjectMocks
+    private ApplicationControllerImpl applicationController;
+    @Mock
+    private PrescoringService prescoringService;
+    @Mock
+    private DealService dealService;
+
+    @Test
+    void testApplicationEndpointOk() {
+        BigDecimal amount = new BigDecimal(10000);
+        Integer term = 6;
+
+        LoanApplicationRequestDTO request = new LoanApplicationRequestDTO();
+        request.setAmount(amount);
+        request.setTerm(term);
+
+        List<LoanOfferDTO> loanOffers = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            LoanOfferDTO loanOfferDTO = new LoanOfferDTO();
+            loanOfferDTO.setRequestedAmount(amount);
+            loanOfferDTO.setTerm(term);
+            loanOffers.add(loanOfferDTO);
+        }
+
+        when(dealService.getLoanOffers(request)).thenReturn(loanOffers);
+
+        ResponseEntity<List<LoanOfferDTO>> result = applicationController.application(request);
+        verify(prescoringService, times(1)).validationOfLoanApplicationRequest(request);
+        assertThat(result.getStatusCode().value()).isEqualTo(200);
+        assertThat(Objects.requireNonNull(result.getBody()).size()).isEqualTo(loanOffers.size());
+        assertThat(result.getBody().getFirst().getRequestedAmount()).isEqualTo(amount);
+        assertThat(result.getBody().getFirst().getTerm()).isEqualTo(term);
+    }
+
+    @Test
+    void testOfferEndpointOk() {
+        LoanOfferDTO loanOfferDTO = new LoanOfferDTO();
+        doNothing().when(dealService).applyOffer(loanOfferDTO);
+        ResponseEntity<Void> result = applicationController.offer(loanOfferDTO);
+        assertThat(result.getStatusCode().value()).isEqualTo(200);
+    }
+}
